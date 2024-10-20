18b978529481b7a92960badeb56e790476ccf84c
DPORTA20 <dporta@360salesolutions.com>
2024-10-20 13:19:08 -0500

    Modificar el servicio de rice crops y agregar servicio de guardado sensor data record

--- src/main/java/com/agrotech/irrigationmanagement/interfaces/rest/resources/CreateSensorDataResource.java
+++ src/main/java/com/agrotech/irrigationmanagement/interfaces/rest/resources/CreateSensorDataResource.java
@@ -2,4 +2,4 @@ package com.agrotech.irrigationmanagement.interfaces.rest.resources;
 
 import java.time.LocalDateTime;
 
-public record CreateSensorDataResource(String lastValue, String typeSensor, Long sensorId) {}
+public record CreateSensorDataResource(Float lastValue, String typeSensor, Long sensorId) {}
